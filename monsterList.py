b693c17d9aadf68306867348118ba7dcb61b16e2
Kelly Tompkins <k.tompkins005@gmail.com>
2018-07-24 12:28:59 -0500

    Create monsterList.py
    Adding file consisting of monster list

---
+++ monsterList.py
@@ -0,0 +1,9 @@
+class Monster:
+  def __init__(self,name,health):
+    self.name = name
+    self.health = health
+
+
+minionMonster = Monster("Minion",10)
+defaultMonster = Monster("Monster", 15)
+bossMonster = Monster("Boss", 25)
