43c0d3879aa7177f8847333871be9f208289db6a
Kelly <k.tompkins005@gmail.com>
2018-08-23 09:13:46 -0500

    adding DiceRoll code to storyline

--- DiceRoll.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-
-attack = random.randint(1,6)
-twentySide = random.randint(1,20)
-
--- storyline.py
+++ storyline.py
@@ -1,2 +1,19 @@
-import sixsided
-import monsterList
+import random
+from monsterList import Monster
+
+
+minionMonster = Monster("Minion",10)
+defaultMonster = Monster("Monster", 15)
+bossMonster = Monster("Boss", 25)
+
+def AttackRoll():
+    attack = random.randint(1,6)
+    print(attack)
+
+
+def twentySided():
+    twentySide = random.randint(1,20)
+
+
+print(minionMonster.health)
+AttackRoll()
