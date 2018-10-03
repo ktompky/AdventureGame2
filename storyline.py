3670f637c7a855ce7be6aa711536530e6a35d9d7
ktompky <k.tompkins005@gmail.com>
2018-10-03 10:07:00 -0500

    Removing extra monsters from storyline

--- storyline.py
+++ storyline.py
@@ -2,9 +2,8 @@ import random
 from monsterList import Monster,Player
 
 
-minionMonster = Monster("Minion",10)
-defaultMonster = Monster("Monster", 15)
-bossMonster = Monster("Boss", 25)
+
+singleMonster = Monster("Monster", 15)
 singlePlayer = Player(input("What is your name? "), 20)
 
 attack = random.randint(1,6)
@@ -13,16 +12,27 @@ initiative = random.randint(1,20)
 def monsterAttack():
     print("It's now the monster's turn!")
     singlePlayer.health -= attack
-    print("You are now at " + str(singlePlayer.health) + " hit points!")
+    print("They attacked you for %s" % attack)
+    print("You are now at %s hit points!" % singlePlayer.health)
 
 
-if initiative >= 10:
-    minionMonster.health -= attack
-    print("You attacked them for: " + str(attack) + " point(s)!")
-    print("The monster is now at: " + str(minionMonster.health) + " hit points!")
-else:
-    print("Evaded!")
-    monsterAttack()
+def playerAttack():
+    print("Hello %s" % singlePlayer.name)
+    if initiative >= 10:
+        print("Your initiative roll was %s so you get to attack! " % initiative)
+        singleMonster.health -= attack
+        print("You attacked them for:  %s point(s)." % attack)
+        print("The monster is now at: %s hit points." % singleMonster.health)
+
+    else:
+        print("Evaded!")
+        print("Your initiative roll was %s " % initiative)
+        monsterAttack()
+
+playerAttack()
+
+
+
 
 
 
