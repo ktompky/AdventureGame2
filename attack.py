862b6c3876ead9a11e99fe641b1cb4df4328ca2a
Kelly <k.tompkins005@gmail.com>
2020-04-29 18:16:47 -0500

    redoing filenames

---
+++ attack.py
@@ -0,0 +1,45 @@
+import random
+from character_list import Monster,Player
+
+
+
+singleMonster = Monster("Monster", 15)
+singlePlayer = Player(input("What is your name? "), 20)
+
+attack = random.randint(1,6)
+initiative = random.randint(1,20)
+
+print("Hello %s" % singlePlayer.name)
+
+def monsterAttack():
+    print("It's now the monster's turn!")
+    singlePlayer.health -= attack
+    print("They attacked you for %s" % attack)
+    print("You are now at %s hit points!" % singlePlayer.health)
+
+
+def playerAttack():
+    
+    if initiative >= 10:
+        print("Your initiative roll was %s so you get to attack! " % initiative)
+        singleMonster.health -= attack
+        print("You attacked them for:  %s point(s)." % attack)
+        print("The monster is now at: %s hit points." % singleMonster.health)
+       
+
+    else:
+        print("Evaded!")
+        print("Your initiative roll was %s " % initiative)
+        monsterAttack()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ character_list.py
@@ -0,0 +1,10 @@
+class Monster:
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
+
+
+class Player:
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
