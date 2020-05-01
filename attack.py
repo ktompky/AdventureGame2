143b170429d8d8411e93f314f6eafc28029a90d5
Kelly <k.tompkins005@gmail.com>
2020-04-30 22:21:13 -0500

    adding attack points

--- attack.py
+++ attack.py
@@ -6,18 +6,20 @@ from character_list import Monster,Player
 singleMonster = Monster("Monster", 15)
 singlePlayer = Player(input("What is your name? "), 20)
 attack = random.randint(1,6)
-
-
-
-print("Hello %s" % singlePlayer.name)
-print("Rolling for initiative....")
 initiative = random.randint(1,20)
+
+
+def startGame():
+    print("Hello %s" % singlePlayer.name)
+    print("Rolling for initiative....")
+
 
 def monsterAttack():
     print("It's now the monster's turn!")
     singlePlayer.health -= attack
     print("They attacked you for %s" % attack)
     print("You are now at %s hit points!" % singlePlayer.health)
+    playerAttack()
 
 
 def playerAttack():
@@ -27,15 +29,20 @@ def playerAttack():
         singleMonster.health -= attack
         print("You attacked them for:  %s point(s)." % attack)
         print("The monster is now at: %s hit points." % singleMonster.health)
-       
+        monsterAttack()
 
     else:
         print("Evaded!")
         print("Your initiative roll was %s " % initiative)
         monsterAttack()
 
+while singleMonster.health and singlePlayer.health != 0:
+    playerAttack()
+    monsterAttack()
 
-
+if __name__ == '__main__':
+    startGame()
+    
 
 
 
--- character_list.py
+++ character_list.py
@@ -2,9 +2,11 @@ class Monster:
     def __init__(self,name,health):
         self.name = name
         self.health = health
+        
 
 
 class Player:
     def __init__(self,name,health):
         self.name = name
         self.health = health
+        
