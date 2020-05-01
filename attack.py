272c3ad15691c6cd5cc1011a633bfc3e71ccc1ed
Kelly <k.tompkins005@gmail.com>
2020-04-30 22:49:40 -0500

    fixing recursive function

--- attack.py
+++ attack.py
@@ -6,12 +6,15 @@ from character_list import Monster,Player
 singleMonster = Monster("Monster", 15)
 singlePlayer = Player(input("What is your name? "), 20)
 
-initiative = random.randint(1,20)
+def roll_initiative():
+    return random.randint(1,20)
 
 
 def startGame():
     print("Hello %s" % singlePlayer.name)
     print("Rolling for initiative....")
+    initiative = roll_initiative()
+    print("Your initiative roll was %s " % initiative)
     if initiative >= 10:
         playerTurn()
     else:
@@ -24,16 +27,24 @@ def monsterTurn():
     singlePlayer.health -= attack
     print("They attacked you for %s" % attack)
     print("You are now at %s hit points!" % singlePlayer.health)
-    playerTurn()
+    if singlePlayer.health >= 1:
+        playerTurn()
+    else:
+        print("You've been killed.")
 
 
 def playerTurn():
-    print("Your initiative roll was %s so you get to attack! " % initiative)
+    print("It's your turn!")
     attack = random.randint(1,6)
     singleMonster.health -= attack
     print("You attacked them for:  %s point(s)." % attack)
     print("The monster is now at: %s hit points." % singleMonster.health)
-    monsterTurn()
+    if singleMonster.health >= 1:
+        monsterTurn()
+    else:
+        print("You killed it.")
+
+
 
 
     
